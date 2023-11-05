7cd6db9b8f39b6634340b457953e08e4cb5f40c1
Emil Karlen <emil.karlen@fripost.org>
2023-11-05 17:27:32 +0100

    IMPL detection of existing target

--- src/ext2lower.py
+++ src/ext2lower.py
@@ -2,7 +2,7 @@ import shutil
 import sys
 import time
 from pathlib import Path
-from typing import Optional, Dict, Sequence, Tuple
+from typing import Optional, Dict, Sequence, Tuple, Callable
 
 CMD_EXE = 'x'
 CMD_DRY = 'd'
@@ -37,26 +37,44 @@ class Reporter:
         self._channel = channel
 
     def report(self, src: Path, dst: Optional[Path]):
-        self._channel.write(str(src) + '\n')
+        if dst is None:
+            self._channel.write(str(src) + '\n')
+        else:
+            self._channel.write('{} -> {}\n'.format(src, dst))
 
 
 def _cli_execute():
+    def renamer(src: Path, dst: Path):
+        if dst.exists():
+            exit_failure('Target exists: {} -> {}'.format(src, dst))
+
+        try:
+            src.rename(dst)
+        except OSError as ex:
+            print('Failed to rename: {} -> {}'.format(src, dst), file=sys.stderr)
+            exit_failure(str(ex))
+
     reporter = Reporter(sys.stdout)
-    _do_it(reporter)
+    _do_it(reporter, renamer)
     _cli_dry()
 
 
 def _cli_dry():
+    def renamer(src: Path, dst: Path):
+        pass
+
     reporter = Reporter(sys.stderr)
-    _do_it(reporter)
+    _do_it(reporter, renamer)
 
 
-def _do_it(reporter: Reporter):
+def _do_it(reporter: Reporter, renamer: Callable[[Path, Path], None]):
     for path_str in sys.stdin.readlines():
         src_path = Path(path_str.strip())
         if not src_path.exists():
             exit_failure('File does not exist: {}'.format(src_path))
         dst_path = new_name(src_path)
+        if dst_path is not None:
+            renamer(src_path, dst_path)
         reporter.report(src_path, dst_path)
 
 
